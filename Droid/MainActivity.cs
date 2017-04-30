63308d191f24df35f32e1508a1b041d84cbbe3b6
Yahya Gilany <yahya.gilany@live.com>
2017-04-30 00:59:09 -0400

    theme issues

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,14 +10,11 @@ using Android.OS;
 
 namespace Phoneword.Droid
 {
-    [Activity(Label = "Phoneword.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "Phoneword", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
